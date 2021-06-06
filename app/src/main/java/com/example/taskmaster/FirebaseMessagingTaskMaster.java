855a8dcc8163e45b63bbac9c61d5a67d51bb5e02
sabbagh99 <a.sameer1999@gmail.com>
2021-06-06 15:25:01 +0300

    add lab 38

---
+++ app/src/main/java/com/example/taskmaster/FirebaseMessagingTaskMaster.java
@@ -0,0 +1,10 @@
+package com.example.taskmaster;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FirebaseMessagingTaskMaster extends FirebaseMessagingService {
+
+}
